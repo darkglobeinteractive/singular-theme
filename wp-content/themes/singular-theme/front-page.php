5abf4934be7009722d90902dc7d967568270f0ca
Robbie Schneider <zerodegreesk@hotmail.com>
2019-08-06 13:00:23 -0400

    Adding mmenu library, updating a few other things. Fixing a coding error in functions.php

---
+++ wp-content/themes/singular-theme/front-page.php
@@ -0,0 +1,6 @@
+<!-- front-page.php -->
+<?php get_header(); ?>
+
+<p>Site coming soon to a web browser near you.</p>
+
+<?php get_footer(); ?>
--- wp-content/themes/singular-theme/functions.php
+++ wp-content/themes/singular-theme/functions.php
@@ -9,28 +9,27 @@ add_theme_support( 'menus' );
 
 
 /* STYLES ------------------------------------------------------- */
-function cst_styles() {
-  wp_enqueue_style( 'custom-google-fonts', 'https://fonts.googleapis.com/css?family=Montserrat:200,200i,300,300i,400,400i,500,600,700', false );
+function singular_styles() {
+  wp_enqueue_style( 'custom-google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:300,300i,700', false );
   // wp_enqueue_style( 'custom-fontastic-icons', 'https://file.myfontastic.com/U9FqFSWAktJNmqp9Dd93ZG/icons.css', false );
   // wp_enqueue_style( 'local-fontastic-icons', get_template_directory_uri().'/fonts/styles.css' );
   // wp_enqueue_style( 'colorbox_css', get_template_directory_uri().'/libs/colorbox/example2/colorbox.css' );
   // wp_enqueue_style( 'slickjs_css', get_template_directory_uri().'/libs/slick-1.8.1/slick/slick.css' );
   // wp_enqueue_style( 'slickjs_theme_css', get_template_directory_uri().'/libs/slick-1.8.1/slick/slick-theme.css' );
-  // wp_enqueue_style( 'mmenu_css', get_template_directory_uri().'/libs/jQuery.mmenu-master/dist/jquery.mmenu.all.css' );
+  wp_enqueue_style( 'mmenu_css', get_template_directory_uri().'/libs/mmenu/dist/jquery.mmenu.all.css' );
   wp_enqueue_style( 'global_css', get_template_directory_uri().'/style.css' );
   wp_enqueue_style( 'responsive_css', get_template_directory_uri().'/style-responsive.css' );
-  }
-  add_action( 'wp_enqueue_scripts', 'cst_styles' );
 }
+add_action( 'wp_enqueue_scripts', 'singular_styles' );
 
 
 /* SCRIPTS ------------------------------------------------------ */
-function cst_scripts() {
+function singular_scripts() {
   // wp_enqueue_script( 'slickjs_js', get_template_directory_uri().'/libs/slick-1.8.1/slick/slick.min.js', array( 'jquery' ) );
-  // wp_enqueue_script( 'mmenu_js', get_template_directory_uri().'/libs/jQuery.mmenu-master/dist/jquery.mmenu.all.js', array( 'jquery' ) );
+  wp_enqueue_script( 'mmenu_js', get_template_directory_uri().'/libs/mmenu/dist/jquery.mmenu.all.js', array( 'jquery' ) );
 	wp_enqueue_script( 'global_js', get_template_directory_uri().'/js/global.js', array( 'jquery', 'slickjs_js' ) );
 }
-add_action( 'wp_enqueue_scripts', 'cst_scripts' );
+add_action( 'wp_enqueue_scripts', 'singular_scripts' );
 
 
 /* WP CORE CUSTOM FIELDS ---------------------------------------- */
