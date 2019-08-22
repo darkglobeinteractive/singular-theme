8d2f93a94f9c4b62456efe5fb520baa4bd35f69b
Robbie Schneider <zerodegreesk@hotmail.com>
2019-08-21 21:00:16 -0400

    Adding custom title functionality to header.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Singular Theme Installation Notes
 ===================================
 
-mmenu (v.7.2.2)
+mmenu (v.7.3.3)
 -----------------------------------
 Based-on the documentation (https://mmenujs.com/documentation/) as of August 6, 2019, we are using mmenu Version 7 here due to Version 8 only supporting ES6 compliant browsers. This would not be fully supportive of IE 10 and 11. As much as I would like to stop considering those browsers, this theme is used for freelance development, so it's something I don't want to have to "fix" down the road. For documentation on mmenu Version 7, see http://mmenu.frebsite.nl.
 
@@ -17,6 +17,7 @@ Plugin Installation Notes
 ===================================
 1) [ ] Install "Classic Editor"
 2) [ ] Install "Custom Post Type UI" (https://github.com/WebDevStudios/custom-post-type-ui/)
+3) [ ] Install "Widget Context" (https://wordpress.org/plugins/widget-context/)
 
 
 
--- wp-content/themes/singular-theme/templates/banner.php
+++ wp-content/themes/singular-theme/templates/banner.php
@@ -2,9 +2,31 @@
 // Grab global variables from custom function.php function
 $gv = singular_global_vars();
 
+// A Category ID indicates this is a category listing page
+$cat_id = ( is_object( $gv['queried_object'] ) ? $gv['queried_object']->cat_ID : '' );
+
+// Set default page title
+$banner_title = wp_title( '', false );
+
+// Customize page title when necessary
+// Blog category listing pages
+if ( $cat_id !== '' ) {
+  $banner_title = 'Blog: '.$gv['queried_object']->name;
+// All blog posts have the same title
+} elseif ( get_post_type() === 'post' ) {
+  $banner_title = 'Blog';
+// Override title if Custom Fields custom_page_title exists
+} elseif ( get_field( 'custom_page_title', $gv['queried_object'] ) ) {
+  $banner_title = get_field( 'custom_page_title', $gv['queried_object'] );
+}
+
+// Build the title element
+$banner_element = ( get_post_type() === 'post' ? '<div class="banner-title">' : '<h1 class="banner-title">' );
+$banner_element .= $banner_title;
+$banner_element .= ( get_post_type() === 'post' ? '</div>' : '</h1>' );
 ?>
 <div id="banner">
   <div class="content">
-    <p>This is the banner</p>
+    <?php echo $banner_element; ?>
   </div>
 </div>
