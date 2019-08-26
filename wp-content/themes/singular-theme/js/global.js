772307c4837df2f4aef2b4717d4c4212a5802d71
Robbie Schneider <zerodegreesk@hotmail.com>
2019-08-26 10:11:16 -0400

    Adding Fixed class to WP Admin Header for mmenu compatibility.

--- wp-content/themes/singular-theme/js/global.js
+++ wp-content/themes/singular-theme/js/global.js
@@ -1,5 +1,9 @@
 jQuery(document).ready(function($) {
 
+  /* ADD FIXED CLASS TO WORDPRESS ADMIN BAR FOR MMENU COMPATIBILITY */
+  $('#wpadminbar').addClass('Fixed');
+
+
   /* MOBILE NAVIGATION ------------------------------------------ */
   $('#navigation > ul.menu').each(function() {
     var $mobile_menu = $(this).clone();
@@ -24,10 +28,12 @@ jQuery(document).ready(function($) {
     });
   });
 
+
   /* SLICK SLIDER ----------------------------------------------- */
   /* Un-comment the slick items in functions.php
   $('.slick-slider').slick();
   */
+
 
   /* SMOOTH SCROLLING ------------------------------------------- */
   $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(t){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var e=$(this.hash);(e=e.length?e:$("[name="+this.hash.slice(1)+"]")).length&&(t.preventDefault(),$("html, body").animate({scrollTop:e.offset().top},1e3,function(){var t=$(e);if(t.focus(),t.is(":focus"))return!1;t.attr("tabindex","-1"),t.focus()}))}});
