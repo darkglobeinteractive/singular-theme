74f806735b4ac1f3b8238eef2630a027b8fbd120
Robbie Schneider <zerodegreesk@hotmail.com>
2020-01-03 12:14:44 -0500

    Adding a 404 page template.

---
+++ wp-content/themes/singular-theme/404.php
@@ -0,0 +1,12 @@
+<!-- 404.php // Page Not Found Template -->
+<?php get_header(); ?>
+
+  <div id="content">
+
+    <div class="404-message">Sorry, but the page you're looking for cannot be found. Please, try again or use the following form to perform a search.</div>
+
+    <?php get_search_form(); ?>
+
+  </div>
+
+<?php get_footer(); ?>
