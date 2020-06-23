fe075211cd521894565c3837a9cb15ceb9690446
Robbie Schneider <zerodegreesk@hotmail.com>
2020-06-23 10:46:52 -0400

    Fixing bogus class name in 404 page and changing element to a paragraph.

--- wp-content/themes/singular-theme/404.php
+++ wp-content/themes/singular-theme/404.php
@@ -3,7 +3,7 @@
 
   <div id="content">
 
-    <div class="404-message">Sorry, but the page you're looking for cannot be found. Please, try again or use the following form to perform a search.</div>
+    <p class="page-not-found">Sorry, but the page you're looking for cannot be found. Please, try again or use the following form to perform a search.</p>
 
     <?php get_search_form(); ?>
 
