a57f09d463dd28252327b8038926d04ea6520bad
Robbie Schneider <zerodegreesk@hotmail.com>
2020-08-18 15:22:13 -0400

    Tweaking search form.

--- wp-content/themes/singular-theme/searchform.php
+++ wp-content/themes/singular-theme/searchform.php
@@ -1,5 +1,5 @@
 <form role="search" method="get" id="searchform" class="searchform" action="/">
-	<div>
+	<div class="searchform-wrap">
 		<label class="screen-reader-text" for="s">Search for:</label>
 		<input type="text" value="<?php echo ( get_search_query() ? get_search_query() : '' ); ?>" name="s" id="s">
 		<input type="submit" id="searchsubmit" value="Search">
