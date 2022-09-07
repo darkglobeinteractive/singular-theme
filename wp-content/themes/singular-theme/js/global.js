34ace2f4896f87fd8c7ce8958011d0518008e8aa
Robbie Schneider <darkglobeinteractive@gmail.com>
2022-09-07 13:11:20 -0400

    Smooth Scrolling for Fixed Elements
    Tweaked the Smooth Scrolling script to add the ability to compensate for fixed elements, like the #wpadminheader and a fixed header. Otherwise, the page scrolls the element to the very top of the page and then be obscured by the fixed elements.

--- wp-content/themes/singular-theme/js/global.js
+++ wp-content/themes/singular-theme/js/global.js
@@ -119,6 +119,20 @@ jQuery(document).ready(function($) {
 
 
   /* SMOOTH SCROLLING ------------------------------------------- */
-  $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(t){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var e=$(this.hash);(e=e.length?e:$("[name="+this.hash.slice(1)+"]")).length&&(t.preventDefault(),$("html, body").animate({scrollTop:e.offset().top},1e3,function(){var t=$(e);if(t.focus(),t.is(":focus"))return!1;t.attr("tabindex","-1"),t.focus()}))}});
+  $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(t){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){
+
+    var e = $(this.hash);
+
+    // Determine the heights of possible elements on the page to take into consideration
+    var header_height = 0; // Fixed headers need to be taken into consideration
+    var wpadminbar_height = ($('#wpadminbar').length > 0 ? $('#wpadminbar').innerHeight() : 0);
+    var scroll_top_padding = 20;
+
+    // Calculate the final offset for scrolling by removing the height of the items above (and some padding) from the total offset height
+    var e_offset_top = e.offset().top - (header_height + wpadminbar_height + scroll_top_padding);
+
+  (e=e.length?e:$("[name="+this.hash.slice(1)+"]")).length&&(t.preventDefault(),$("html, body").animate({
+    scrollTop:e_offset_top
+  },1e3,function(){var t=$(e);if(t.focus(),t.is(":focus"))return!1;t.attr("tabindex","-1"),t.focus()}))}});
 
 });
