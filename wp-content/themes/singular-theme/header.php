e8172264dd8e7d73084d074acd22d252aa7b715e
Robbie Schneider <darkglobeinteractive@gmail.com>
2022-09-16 14:09:16 -0400

    Changing Skip To Content Target
    Changed the skip to content link to actually link to the #main element, rather than the #banner. #banner is usually the first thing after the navigation and can contain the h1 tag, but sometimes the banner isn't used or the anchor ID can change, causing accessibility issues.

--- wp-content/themes/singular-theme/header.php
+++ wp-content/themes/singular-theme/header.php
@@ -24,7 +24,7 @@
 	  array_push ( $additional_classes, 'sidebar' );
 	}
 	body_class( $additional_classes ); ?>>
-		<a href="#banner" class="skip-main">Skip to Main Content</a>
+		<a href="#main" class="skip-main">Skip to Main Content</a>
     <div id="page">
       <header id="header">
 				<div class="wrap">
