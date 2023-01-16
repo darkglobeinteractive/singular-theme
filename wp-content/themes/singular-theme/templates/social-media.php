a8d2c4e4fd0fa402336989267726ca2fc7acc422
Robbie Schneider <darkglobeinteractive@gmail.com>
2023-01-16 12:52:00 -0500

    Adding aria-labelledby attribute for accessibilityor

--- wp-content/themes/singular-theme/templates/social-media.php
+++ wp-content/themes/singular-theme/templates/social-media.php
@@ -2,7 +2,7 @@
   <li class="icon facebook">
     <a href="#" target="_blank" rel="noopener" aria-label="Facebook Link">
       <span class="label">Facebook</span>
-      <svg role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <svg role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-labelledby="title">
         <title>Facebook Icon</title>
         <path class="fill" d="M227.954,24h0Zm.085,0h0Zm-.121,0h0Zm.16,0h0Zm-.2,0h0Zm.234,0h0Zm.039,0h0Zm-.318,0h0Zm.356,0h0Zm-.394,0h0Zm.433,0h0Zm-.468,0h0Zm-.044,0h0Zm.551,0h0Zm-.589,0h0Zm.628,0h0Zm.039,0h0Zm-.7,0h0Zm-.034,0h0Zm.774,0h0Zm.038,0h0Zm-.861,0h0Zm-.035,0h0Zm.935,0h0Zm-.969,0h0Zm1.008,0h0Zm-1.056,0h0Zm1.094,0h0Zm-1.129,0h0Zm1.167,0h0Zm-1.2,0h0Zm1.24,0h0Zm.038,0h0Zm-1.309,0h0Zm1.347,0h0Zm-1.4,0h0Zm-.034,0h0Zm1.471,0h0Zm-1.5,0h0Zm1.542,0h0Zm.038,0h0Zm-1.629,0h0Zm-.034,0h0Zm1.7,0h0Zm-1.735,0h0Zm1.773,0h0Zm-1.818,0h0Zm1.856,0h0Zm.038,0h0Zm-1.929,0h0Zm-.034,0h0Zm2,0h0Zm-2.039,0h0Zm2.077,0h0Zm.038,0h0Zm-2.155,0h0Zm-.035,0h0Zm2.228,0h0Zm-2.988-.095a12,12,0,1,1,3.75,0V15.469h2.8L233.2,12h-3.328V9.749a1.734,1.734,0,0,1,1.956-1.874h1.513V4.922a18.452,18.452,0,0,0-2.686-.234c-2.741,0-4.533,1.661-4.533,4.669V12h-3.047v3.469h3.047Z" transform="translate(-216)" fill-rule="evenodd"/>
       </svg>
@@ -11,7 +11,7 @@
   <li class="icon linkedin">
     <a href="#" target="_blank" rel="noopener" aria-label="LinkedIn Link">
       <span class="label">LinkedIn</span>
-      <svg role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <svg role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-labelledby="title">
         <title>LinkedIn Icon</title>
         <path class="fill" d="M237.068,0A2.934,2.934,0,0,1,240,2.932V21.068A2.934,2.934,0,0,1,237.068,24H218.932A2.934,2.934,0,0,1,216,21.068V2.932A2.934,2.934,0,0,1,218.932,0ZM223.522,19.841V9.261H220v10.58Zm12.682,0V13.774c0-3.25-1.735-4.762-4.049-4.762a3.493,3.493,0,0,0-3.17,1.747v-1.5h-3.517c.047.993,0,10.58,0,10.58h3.517V13.932a2.4,2.4,0,0,1,.116-.859,1.925,1.925,0,0,1,1.8-1.286c1.272,0,1.782.971,1.782,2.392v5.661ZM221.787,4.159a1.833,1.833,0,1,0-.047,3.656h.023a1.834,1.834,0,1,0,.024-3.656Z" transform="translate(-216)" fill-rule="evenodd"/>
       </svg>
@@ -20,7 +20,7 @@
   <li class="icon twitter">
     <a href="#" target="_blank" rel="noopener" aria-label="Twitter Link">
       <span class="label">Twitter</span>
-      <svg role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <svg role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-labelledby="title">
         <title>Twitter Icon</title>
         <path class="fill" data-name="Path 4" d="M228,0a12,12,0,1,1-12,12A12.006,12.006,0,0,1,228,0Zm-2.114,18.382a8.158,8.158,0,0,0,8.214-8.214c0-.126,0-.251-.006-.371a5.913,5.913,0,0,0,1.443-1.5,5.859,5.859,0,0,1-1.658.455,2.885,2.885,0,0,0,1.269-1.6,5.846,5.846,0,0,1-1.832.7,2.888,2.888,0,0,0-4.993,1.976,2.647,2.647,0,0,0,.078.659,8.189,8.189,0,0,1-5.951-3.017,2.885,2.885,0,0,0,.9,3.849,2.836,2.836,0,0,1-1.305-.359V11a2.89,2.89,0,0,0,2.317,2.832,2.88,2.88,0,0,1-.76.1,2.754,2.754,0,0,1-.545-.054,2.882,2.882,0,0,0,2.694,2.005,5.8,5.8,0,0,1-3.586,1.233,5.253,5.253,0,0,1-.688-.042,8.043,8.043,0,0,0,4.412,1.305Z" transform="translate(-216)" fill-rule="evenodd"/>
       </svg>
@@ -29,7 +29,7 @@
   <li class="icon youtube">
     <a href="#" target="_blank" rel="noopener" aria-label="YouTube Link">
       <span class="label">YouTube</span>
-      <svg role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <svg role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-labelledby="title">
         <title>YouTube Icon</title>
         <path class="fill" d="M237.068,0A2.934,2.934,0,0,1,240,2.932V21.068A2.934,2.934,0,0,1,237.068,24H218.932A2.934,2.934,0,0,1,216,21.068V2.932A2.934,2.934,0,0,1,218.932,0Zm-.983,8.233a2.113,2.113,0,0,0-1.492-1.492,61.8,61.8,0,0,0-13.186,0,2.113,2.113,0,0,0-1.492,1.492,21.963,21.963,0,0,0-.353,4.062,21.961,21.961,0,0,0,.353,4.062,2.114,2.114,0,0,0,1.492,1.492A50.445,50.445,0,0,0,228,18.2a50.445,50.445,0,0,0,6.593-.353,2.113,2.113,0,0,0,1.492-1.492,21.964,21.964,0,0,0,.353-4.062A21.966,21.966,0,0,0,236.085,8.233Zm-9.773,6.593V9.763l4.384,2.531-4.384,2.531Z" transform="translate(-216)" fill-rule="evenodd"/>
       </svg>
