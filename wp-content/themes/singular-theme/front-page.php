6f14eaf5b935428fac525e18616994f7cbd8dad1
Robbie Schneider <darkglobeinteractive@gmail.com>
2024-05-31 14:12:24 -0400

    Removing default page title and page content conditionals that I've never needed.

--- wp-content/themes/singular-theme/front-page.php
+++ wp-content/themes/singular-theme/front-page.php
@@ -5,15 +5,9 @@
 
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-    <?php if ( get_the_title() || get_field( 'custom_page_title' ) ): ?>
-      <h1><?php echo ( get_field( 'custom_page_title' ) ? get_field( 'custom_page_title' ) : get_the_title() ); ?></h1>
-    <?php endif; ?>
-
     <?php the_content(); ?>
 
-  <?php endwhile; else: ?>
-    <p>We cannot find the content you are looking for. Please, try again.</p>
-  <?php endif; ?>
+  <?php endwhile; endif; ?>
 
 </div>
 
