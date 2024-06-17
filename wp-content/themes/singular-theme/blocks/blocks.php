865103f2ccc5485a84d893731d68b22ff77db282
Robbie Schneider <darkglobeinteractive@gmail.com>
2024-06-17 16:53:51 -0400

    Moving custom block-based JS to a shared JS file.

--- wp-content/themes/singular-theme/blocks/blocks.php
+++ wp-content/themes/singular-theme/blocks/blocks.php
@@ -23,8 +23,8 @@ function singular_custom_block_styles() {
   wp_enqueue_style( 'color-block-cta-css', get_template_directory_uri().'/blocks/color-block-cta/color-block-cta.css', array(), singular_theme_filemtime( '/blocks/color-block-cta/color-block-cta.css' ) );
   wp_enqueue_style( 'image-cta-css', get_template_directory_uri().'/blocks/image-cta/image-cta.css', array(), singular_theme_filemtime( '/blocks/image-cta/image-cta.css' ) );
   wp_enqueue_style( 'multi-slider-css', get_template_directory_uri().'/blocks/multi-slider/multi-slider.css', array(), singular_theme_filemtime( '/blocks/multi-slider/multi-slider.css' ) );
-  wp_enqueue_script( 'multi-slider-js', get_template_directory_uri().'/blocks/multi-slider/multi-slider.js', array( 'jquery' ), singular_theme_filemtime('/blocks/multi-slider/multi-slider.js' ) );
   wp_enqueue_style( 'testimonial-css', get_template_directory_uri().'/blocks/testimonial/testimonial.css', array(), singular_theme_filemtime( '/blocks/testimonial/testimonial.css' ) );
+  wp_enqueue_script( 'custom-blocks-js', get_template_directory_uri().'/blocks/blocks.js', array( 'jquery' ), singular_theme_filemtime('/blocks/blocks.js' ) );
 }
 add_action( 'wp_enqueue_scripts', 'singular_custom_block_styles' );
 
